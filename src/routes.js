4d064a2628890b8418bb9cea99166ceea227c5ab
Antony Budianto <antonybudianto@gmail.com>
2016-12-31 21:56:56 +0700

    refactor firebase auth middleware

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,13 @@
-const admin = require('firebase-admin');
 const express = require('express');
 const router = express.Router();
-const logger = require('./utils/logger');
+
+const firebaseAuthMiddleware = require('./middlewares/firebase-auth.middleware');
 
 router.use((req, res, next) => {
     next();
 });
 
-router.use('/api', (req, res, next) => {
-    let authorization = req.header('Authorization');
-    if (authorization) {
-        let token = authorization.split(' ');
-        admin.auth().verifyIdToken(token[1])
-        .then((decodedToken) => {
-            res.locals.user = decodedToken;
-            next();
-        })
-        .catch(err => {
-            logger.log(err);
-            res.sendStatus(401);
-        });
-    } else {
-        logger.log('Authorization Header not found');
-        res.sendStatus(401);
-    }
-});
+router.use('/api', firebaseAuthMiddleware);
 
 router.get('/', (req, res) => {
     res.json({
